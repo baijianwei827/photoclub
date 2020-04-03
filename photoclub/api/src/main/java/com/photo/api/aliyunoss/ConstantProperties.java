2f502f6252e1054685875d55a85483d810aba289
一粒丿咖啡豆 <zw2214@163.com>
2020-04-03 17:36:24 +0800

    Update ConstantProperties.java

--- photoclub/api/src/main/java/com/photo/api/aliyunoss/ConstantProperties.java
+++ photoclub/api/src/main/java/com/photo/api/aliyunoss/ConstantProperties.java
@@ -4,8 +4,8 @@ package com.photo.api.aliyunoss;
 public class ConstantProperties  {
 
     public static final String END_POINT  = "oss-cn-shanghai.aliyuncs.com"     ;
-    public static final String ACCESS_KEY_ID  = "LTAICvcVlQwDemcT" ;
-    public static final String ACCESS_KEY_SECRET = "KMbQKcFhNMdEU1O8hX1DswWJSNMjSY";
+    public static final String ACCESS_KEY_ID  = "" ;
+    public static final String ACCESS_KEY_SECRET = "";
     public static final String BUCKET_NAME  = "photoclub";
     public static final String FILE_HOST  = "images";
 
